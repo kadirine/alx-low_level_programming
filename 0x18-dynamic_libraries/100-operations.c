3b8ed6b3405c6d8d0da2b64ecbb11071c3ce173c
kadirine <mohamedmo3iz99@gmail.com>
2023-11-13 17:45:29 -0800

    welcome to 0x18. C - Dynamic libraries project

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,47 +1,32 @@
 #include <stdio.h>
 
-// Addition
-int add(int a, int b)
-{
-return (a + b);
+int add(int a, int b) {
+    return a + b;
 }
 
-// Subtraction
-int sub(int a, int b)
-{
-return (a - b);
+int sub(int a, int b) {
+    return a - b;
 }
 
-// Multiplication
-int mul(int a, int b)
-{
-return (a * b);
+int mul(int a, int b) {
+    return a * b;
 }
 
-// Division
-int divi(int a, int b)
-{
-if (b != 0)
-{
-return (a / b);
-}
-else
-{
-printf("Error: Division by zero\n");
-return (0);
-}
+int div(int a, int b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        printf("Error: Division by zero.\n");
+        return 0;
+    }
 }
 
-// Modulus
-int mod(int a, int b)
-{
-if (b != 0)
-{
-return (a % b);
+int mod(int a, int b) {
+    if (b != 0) {
+        return a % b;
+    } else {
+        printf("Error: Modulo by zero.\n");
+        return 0;
+    }
 }
-else
-{
-printf("Error: Modulus by zero\n");
-return (0);
-}
-}
+
